8dea05cc82f04e1ebbf6e3e3e758de55a49ce35f
muiz owolabi <beasteye.official@gmail.com>
2025-04-28 09:39:45 +0100

    fix: enhance Navbar functionality with improved mobile menu and scroll handling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Link, NavLink, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   IoIosArrowDown,
@@ -58,6 +58,16 @@ const Navbar = () => {
     }, 0);
     return `$${total.toFixed(2)}`;
   };
+  useEffect(() => {
+    if (cartOpen || isOpen) {
+      document.body.style.overflow = "hidden"; // Disable scroll
+    } else {
+      document.body.style.overflow = "auto"; // Enable scroll back
+    }
+    return () => {
+      document.body.style.overflow = "auto"; // Cleanup when unmount
+    };
+  }, [cartOpen, isOpen]);
 
   return (
     <div className="relative">
@@ -181,91 +191,100 @@ const Navbar = () => {
       {/* The Mobile Menu */}
       <AnimatePresence>
         {isOpen && (
-          <motion.div
-            initial={{ x: "-100%" }}
-            animate={{ x: 0 }}
-            exit={{ x: "-100%" }}
-            transition={{ duration: 0.3 }}
-            className="fixed top-0 left-0 w-[70%] h-full bg-white text-black py-4 z-50 shadow-lg md:hidden overflow-auto"
-          >
-            <div className="flex justify-between items-center mb-8 px-3">
-              <Link to={"/"}>
-                <Logo className="w-[30%]" />
-              </Link>
-              <button onClick={() => setIsOpen(false)}>
-                <IoClose size={24} />
-              </button>
-            </div>
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.5 }}
+              exit={{ opacity: 0 }}
+              onClick={() => setIsOpen(false)}
+              className="fixed inset-0 bg-black z-40"
+            />
+            <motion.div
+              initial={{ x: "-100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ duration: 0.3 }}
+              className="fixed top-0 left-0 w-[70%] h-full bg-white text-black py-4 z-50 shadow-lg md:hidden overflow-auto"
+            >
+              <div className="flex justify-between items-center mb-8 px-3">
+                <Link to={"/"}>
+                  <Logo className="w-[30%]" />
+                </Link>
+                <button onClick={() => setIsOpen(false)}>
+                  <IoClose size={24} />
+                </button>
+              </div>
 
-            <ul className="space-y-6 text-sm font-bold border-t border-gray-300 pt-5">
-              {navLinks.map((item: NavLinkType, index) => (
-                <li key={index} className="border-b border-gray-300 pb-4">
-                  {"dropdown" in item ? (
-                    <>
-                      <button
-                        onClick={() =>
-                          setActiveDropdown((prev) =>
-                            prev === item.label ? null : item.label
-                          )
-                        }
-                        className="flex justify-between items-center w-full py-2 px-4"
+              <ul className="space-y-6 text-sm font-bold border-t border-gray-300 pt-5">
+                {navLinks.map((item: NavLinkType, index) => (
+                  <li key={index} className="border-b border-gray-300 pb-4">
+                    {"dropdown" in item ? (
+                      <>
+                        <button
+                          onClick={() =>
+                            setActiveDropdown((prev) =>
+                              prev === item.label ? null : item.label
+                            )
+                          }
+                          className="flex justify-between items-center w-full py-2 px-4"
+                        >
+                          {item.label}
+                          <span>
+                            {activeDropdown === item.label ? (
+                              <IoIosArrowDropupCircle />
+                            ) : (
+                              <IoIosArrowDropdown />
+                            )}
+                          </span>
+                        </button>
+
+                        <AnimatePresence>
+                          {activeDropdown === item.label && (
+                            <motion.ul
+                              initial={{ height: 0 }}
+                              animate={{ height: "auto" }}
+                              exit={{ height: 0 }}
+                              className="overflow-hidden flex flex-col pl-6 gap-4 text-gray-600 font-normal mt-2"
+                            >
+                              {item.dropdown?.map(
+                                (group: any, groupIdx: number) => (
+                                  <div key={groupIdx}>
+                                    <h4 className="text-[#FF90BB] text-sm mb-1">
+                                      {group.category}
+                                    </h4>
+                                    {group.links.map(
+                                      (link: any, linkIdx: number) => (
+                                        <NavLink
+                                          key={linkIdx}
+                                          to={link.path}
+                                          className="block py-1"
+                                          onClick={() => setIsOpen(false)}
+                                        >
+                                          {link.label}
+                                        </NavLink>
+                                      )
+                                    )}
+                                  </div>
+                                )
+                              )}
+                            </motion.ul>
+                          )}
+                        </AnimatePresence>
+                      </>
+                    ) : (
+                      <NavLink
+                        to={item.path!}
+                        className="block py-2 px-4"
+                        onClick={() => setIsOpen(false)}
                       >
                         {item.label}
-                        <span>
-                          {activeDropdown === item.label ? (
-                            <IoIosArrowDropupCircle />
-                          ) : (
-                            <IoIosArrowDropdown />
-                          )}
-                        </span>
-                      </button>
-
-                      <AnimatePresence>
-                        {activeDropdown === item.label && (
-                          <motion.ul
-                            initial={{ height: 0 }}
-                            animate={{ height: "auto" }}
-                            exit={{ height: 0 }}
-                            className="overflow-hidden flex flex-col pl-6 gap-4 text-gray-600 font-normal mt-2"
-                          >
-                            {item.dropdown?.map(
-                              (group: any, groupIdx: number) => (
-                                <div key={groupIdx}>
-                                  <h4 className="text-[#FF90BB] text-sm mb-1">
-                                    {group.category}
-                                  </h4>
-                                  {group.links.map(
-                                    (link: any, linkIdx: number) => (
-                                      <NavLink
-                                        key={linkIdx}
-                                        to={link.path}
-                                        className="block py-1"
-                                        onClick={() => setIsOpen(false)}
-                                      >
-                                        {link.label}
-                                      </NavLink>
-                                    )
-                                  )}
-                                </div>
-                              )
-                            )}
-                          </motion.ul>
-                        )}
-                      </AnimatePresence>
-                    </>
-                  ) : (
-                    <NavLink
-                      to={item.path!}
-                      className="block py-2 px-4"
-                      onClick={() => setIsOpen(false)}
-                    >
-                      {item.label}
-                    </NavLink>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </motion.div>
+                      </NavLink>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          </>
         )}
       </AnimatePresence>
 
